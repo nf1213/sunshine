e6c2e4db82824741fbfff57253a76a6b680c45ec
Nicole <nfelch121394@gmail.com>
2015-03-03 15:19:16 -0500

    Share day's forecast with ShareActionProvider

--- app/src/main/java/com/example/kitten/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/kitten/sunshine/app/DetailActivity.java
@@ -1,14 +1,19 @@
 package com.example.kitten.sunshine.app;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 
 
@@ -25,8 +30,6 @@ public class DetailActivity extends ActionBarActivity {
         }
     }
 
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
@@ -53,22 +56,54 @@ public class DetailActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
-
-        TextView textView;
+        private ShareActionProvider mShareActionProvider;
+        private String mForecastString;
 
         public PlaceholderFragment() {
         }
 
         @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setHasOptionsMenu(true);
+        }
+
+        @SuppressLint("NewApi")
+        @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.detail_text_view);
             Intent intent = getActivity().getIntent();
             if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((TextView) rootView.findViewById(R.id.detail_text_view)).setText(forecastStr);
+                mForecastString = intent.getStringExtra(Intent.EXTRA_TEXT);
+                textView.setText(mForecastString);
             }
             return rootView;
+        }
+
+        @SuppressLint("NewApi")
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detail_fragment, menu);
+            // Locate MenuItem with ShareActionProvider
+            MenuItem item = menu.findItem(R.id.action_share);
+            // Fetch and store ShareActionProvider
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            if(mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(shareIntent(mForecastString));
+            } else {
+                Log.v("DetailActivity", "mShareActionProvider is null!!");
+            }
+        }
+
+        private Intent shareIntent(String text) {
+            String hashtag = " #sunshineapp";
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, text + hashtag);
+            return intent;
         }
     }
 }
