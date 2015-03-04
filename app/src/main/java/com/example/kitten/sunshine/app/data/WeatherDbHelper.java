c0151fad8c0d271e4b444be499becc0bb60124be
Nicole <nfelch121394@gmail.com>
2015-03-04 14:54:36 -0500

    Create location table with DbHelper

--- app/src/main/java/com/example/kitten/sunshine/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/kitten/sunshine/app/data/WeatherDbHelper.java
@@ -70,6 +70,18 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
 
         sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+
+        final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
+                LocationEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+
+                LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
+                LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
+                LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
+                LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL " +
+                " );";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_LOCATION_TABLE);
+
     }
 
     @Override
