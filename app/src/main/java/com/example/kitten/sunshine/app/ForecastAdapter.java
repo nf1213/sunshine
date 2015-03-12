5ba4874663a6788ff15ed0c0cb6431ea41807b44
Nicole <nfelch121394@gmail.com>
2015-03-11 23:43:59 -0400

    show today layout for first item in list

--- app/src/main/java/com/example/kitten/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/kitten/sunshine/app/ForecastAdapter.java
@@ -18,6 +18,20 @@ public class ForecastAdapter extends CursorAdapter {
         super(context, c, flags);
     }
 
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
     /**
      * Prepare the weather high/lows for presentation.
      */
@@ -46,7 +60,17 @@ public class ForecastAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+
+        if (viewType == VIEW_TYPE_TODAY){
+            layoutId = R.layout.list_item_forecast_today;
+        }
+        else if (viewType == VIEW_TYPE_FUTURE_DAY){
+            layoutId = R.layout.list_item_forecast;
+        }
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
