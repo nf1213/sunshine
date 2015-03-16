22efa4eacae78c401fa78ab6497cfb4f6533e049
Nicole <nfelch121394@gmail.com>
2015-03-16 12:26:43 -0400

    Use a Sync Adapter

--- app/src/main/java/com/example/kitten/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/kitten/sunshine/app/ForecastFragment.java
@@ -1,9 +1,5 @@
 package com.example.kitten.sunshine.app;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,7 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.kitten.sunshine.app.data.WeatherContract;
-import com.example.kitten.sunshine.app.service.SunshineService;
+import com.example.kitten.sunshine.app.sync.SunshineSyncAdapter;
 
 public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
@@ -160,16 +156,7 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateWeather() {
-        Intent alarmIntent = new Intent(getActivity(), SunshineService.AlarmReceiver.class);
-        alarmIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
-
-        //Wrap in a pending intent which only fires once.
-        PendingIntent pi = PendingIntent.getBroadcast(getActivity(), 0,alarmIntent,PendingIntent.FLAG_ONE_SHOT);//getBroadcast(context, 0, i, 0);
-
-        AlarmManager am=(AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
-
-        //Set the AlarmManager to wake up the system.
-        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pi);
+        SunshineSyncAdapter.syncImmediately(getActivity());
     }
 
     // since we read the location when we create the loader, all we need to do is restart things
---
+++ app/src/main/java/com/example/kitten/sunshine/app/sync/SunshineAuthenticatorService.java
@@ -0,0 +1,28 @@
+package com.example.kitten.sunshine.app.sync;
+
+import android.content.Intent;
+import android.os.IBinder;
+import android.app.Service;
+
+/**
+ * The service which allows the sync adapter framework to access the authenticator.
+ */
+public class SunshineAuthenticatorService extends Service {
+    // Instance field that stores the authenticator object
+    private SunshineAuthenticator mAuthenticator;
+
+    @Override
+    public void onCreate() {
+        // Create a new authenticator object
+        mAuthenticator = new SunshineAuthenticator(this);
+    }
+
+    /*
+     * When the system binds to this Service to make the RPC call
+     * return the authenticator's IBinder.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mAuthenticator.getIBinder();
+    }
+}
