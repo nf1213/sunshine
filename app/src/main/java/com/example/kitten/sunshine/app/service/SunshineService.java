9d40da1a17a46b2f7ef25036b26ae2c8a21e594f
Nicole <nfelch121394@gmail.com>
2015-03-16 11:33:27 -0400

    Use AlarmReceiver and PendingIntent

--- app/src/main/java/com/example/kitten/sunshine/app/service/SunshineService.java
+++ app/src/main/java/com/example/kitten/sunshine/app/service/SunshineService.java
@@ -1,8 +1,10 @@
 package com.example.kitten.sunshine.app.service;
 
 import android.app.IntentService;
+import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -316,6 +318,16 @@ public class SunshineService extends IntentService {
         locationCursor.close();
         // Wait, that worked?  Yes!
         return locationId;
+    }
+
+    public static class AlarmReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent sendIntent = new Intent(context, SunshineService.class);
+            sendIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, intent.getStringExtra(SunshineService.LOCATION_QUERY_EXTRA));
+            context.startService(sendIntent);
+        }
     }
 
 }
